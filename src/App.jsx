1d38c1bb2ec2b0a7f24012be96693540b9c9b912
ChaseB-creator <Chasioboxio@gmail.com>
2026-05-31 15:54:46 -0600

    Adding Chat Room base UI for App.jsx and App.css

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,22 @@ function App() {
 
   return (
     <>
-    <head>
-      This is where the main chat page will be formated and visually represented.
-    </head>
+    <div className="app-container">
+      <div className="sidebar">
+        <h2>Chat Rooms</h2>
+        <p>This is where the chat rooms and navigation will be displayed</p>
+      </div>
+      <div className="main-chat">
+        <div className="chat-messages">
+          <h2>Chat Messages/Display</h2>
+          <p>This is display a scrollable list of messages from each user/s.</p>
+        </div>
+        <div className="message-input">
+          <h2>User Input</h2>
+          <p>The chatbox will go here</p>
+        </div>
+      </div>
+    </div>
     </>
   )
 }
