124fb32fee658ae363830a134567af61792fc698
Byte-Tor-and-the-Codedog <allenbulleigh@gmail.com>
2026-06-17 21:37:35 -0600

    restyled input with mii theme

--- src/App.jsx
+++ src/App.jsx
@@ -302,7 +302,7 @@ function App() {
             <div className="inputAndButton">
               <input value={messageInput} onChange={(e) => setMessageInput(e.target.value)}
                 placeholder="Enter your message here..." />
-              <button type="submit"><img src={sendButton}></img></button>
+              <button type="submit">send</button>
             </div>
           </form>
         </div>
